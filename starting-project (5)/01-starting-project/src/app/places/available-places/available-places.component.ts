0c6a6f5fd3ec64cd1b9f065901bfa950977c2ee6
Mourad Dellali <mourad.dellali@ensia.edu.dz>
2026-04-01 20:50:06 +0100

    sending data, error logging, fetching..

--- starting-project (5)/01-starting-project/src/app/places/available-places/available-places.component.ts
+++ starting-project (5)/01-starting-project/src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -13,18 +14,36 @@ import { HttpClient } from '@angular/common/http';
   imports: [PlacesComponent, PlacesContainerComponent],
 })
 export class AvailablePlacesComponent implements OnInit{
+onSelectPlace(selectedPlace: Place) {
+  this.httpClient.put('http://localhost:3000/user-places', {
+    placeId: selectedPlace.id,
+  }).subscribe({
+    next: (resData) => console.log(resData),
+  });
+}
   places = signal<Place[] | undefined>(undefined);
   private destroyRef = inject(DestroyRef)
-
+  isFetching = signal(false);
   private httpClient = inject(HttpClient)
+  error = signal('');
 
   ngOnInit() {
-   const subscription =  this.httpClient.get<{places: Place[]}>('http://localhost:3000/places', {
-    observe: 'response'
-   }).subscribe({
-      next: (response) => {
-        console.log(response);
-      }
+    this.isFetching.set(true);
+   const subscription =  this.httpClient.get<{places: Place[]}>('http://localhost:3000/places').pipe(
+    map((resData) => resData.places)
+   )
+   .subscribe({
+      next: (places) => {
+        this.places.set(places);
+      },
+      error : (error) => {
+        console.log(error);
+        this.error.set("Something Went Wrong");
+        
+      },
+      complete: () => {
+        this.isFetching.set(false);
+         },
     });
     
   this.destroyRef.onDestroy(() => {
