7b4b5a20f6837f5d7980b69ce6376e13d5728f10
Mourad Dellali <mourad.dellali@ensia.edu.dz>
2026-04-02 22:24:07 +0100

    .

--- starting-project (5)/01-starting-project/src/app/places/available-places/available-places.component.ts
+++ starting-project (5)/01-starting-project/src/app/places/available-places/available-places.component.ts
@@ -5,6 +5,7 @@ import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-available-places',
@@ -14,10 +15,10 @@ import { map } from 'rxjs';
   imports: [PlacesComponent, PlacesContainerComponent],
 })
 export class AvailablePlacesComponent implements OnInit{
+
+  
 onSelectPlace(selectedPlace: Place) {
-  this.httpClient.put('http://localhost:3000/user-places', {
-    placeId: selectedPlace.id,
-  }).subscribe({
+  this.placesService.addPlaceToUserPlaces(selectedPlace.id).subscribe({  
     next: (resData) => console.log(resData),
   });
 }
@@ -26,12 +27,11 @@ onSelectPlace(selectedPlace: Place) {
   isFetching = signal(false);
   private httpClient = inject(HttpClient)
   error = signal('');
+  private placesService = inject(PlacesService);
 
   ngOnInit() {
     this.isFetching.set(true);
-   const subscription =  this.httpClient.get<{places: Place[]}>('http://localhost:3000/places').pipe(
-    map((resData) => resData.places)
-   )
+   const subscription =  this.placesService.loadAvailablePlaces()
    .subscribe({
       next: (places) => {
         this.places.set(places);
@@ -50,5 +50,6 @@ onSelectPlace(selectedPlace: Place) {
     subscription.unsubscribe();
   });
   }
+  
 
 }
--- starting-project (5)/01-starting-project/src/app/places/user-places/user-places.component.ts
+++ starting-project (5)/01-starting-project/src/app/places/user-places/user-places.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { PlacesService } from '../places.service';
 import { PlacesComponent } from '../places.component';
 
 @Component({
@@ -10,5 +11,18 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit{
+  private placesService = inject(PlacesService);
+  private destroyRef = inject(DestroyRef);
+  places = this.placesService.loadedUserPlaces;
+
+  ngOnInit(): void {
+    const subscription = this.placesService.loadUserPlaces().subscribe({
+      next: () => {},
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
